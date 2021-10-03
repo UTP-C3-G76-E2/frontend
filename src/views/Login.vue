2c3302c2b98fa308033390a9eb005d4f69482c89
krito20 <capaciteseparaelexito@gmail.com>
2021-10-03 16:30:55 -0500

    update

---
+++ src/views/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-card id="card-login"
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    >
+      <v-card-title>Administrar página</v-card-title>
+    </v-img><br>
+    <v-form>
+        <v-text-field
+            label="Usuario"
+            placeholder="Usuario"
+            outlined
+          ></v-text-field>
+          <v-text-field
+            label="Contraseña"
+            placeholder="Contraseña"
+            outlined
+            type="password"
+          ></v-text-field>
+    </v-form>
+
+    <v-card-actions>
+      <v-btn
+        color="blue"
+        text
+      >
+        Registrarse
+      </v-btn>
+
+      <v-btn
+        color="blue"
+        text
+        @click="login"
+      >
+        Iniciar Sesión
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+    data: ()=>{
+        return{
+
+        }
+    },
+    methods: {
+        login(){
+            console.log("verificando credeciales");
+            this.$router.push('/productos');
+        }
+    }
+}
+</script>
+
+<style>
+#card-login{
+    margin-top: 5%;
+}
+</style>
